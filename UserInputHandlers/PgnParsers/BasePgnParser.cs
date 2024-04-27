75a955fce51c76aeef7857035a04348b40770c42
unknown <yipy214@gmail.com>
2024-04-27 20:08:52 +0100

    Added IsNum validator function

--- UserInputHandlers/PgnParsers/BasePgnParser.cs
+++ UserInputHandlers/PgnParsers/BasePgnParser.cs
@@ -19,4 +19,12 @@ abstract class BasePgnParser: IPgnParser
             default: return false;
         }
     }
+    private static protected bool IsNum(char c)
+    {
+        switch(c)
+        {
+            case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '0': return true;
+            default: return false;
+        }
+    }
 }
